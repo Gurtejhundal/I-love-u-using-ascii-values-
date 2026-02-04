633aa69994b614c0e3da0c41d6b5c318c4306c4d
Gurtejhundal <149035823+Gurtejhundal@users.noreply.github.com>
2026-02-04 10:57:29 +0530

    Add files via upload

---
+++ I love u.c
@@ -0,0 +1,22 @@
+// I love u coding using ascii values 
+#include <stdio.h>
+int main(){
+    
+    int a=73,b=76,c=79,d=86,e=69,f=85;
+    char z=')',y=':';
+    
+    printf("%c ", a);
+
+    printf("%c", b);
+    printf("%c", c);
+    printf("%c", d);
+    printf("%c ", e);
+  
+    printf("%c ", f);
+ 
+    printf("%c", y);
+    printf("%c", z);
+    
+    return 0;
+
+}
